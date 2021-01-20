61aa82eca84f204ad5779132dd64be5aa3a87025
Bry2n <bryanmoyalima@gmail.com>
2021-01-20 11:50:44 -0800

    cambios para el menu y boton

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,34 @@
+let boton=document.getElementById("icono");
+let botonCerrar=document.getElementById("boton-cerrar");
+let enlaces=document.getElementById("menu");
+let contador=0;
 
-window.onclick = function (event) {
-    const dropdowns = document.getElementsByClassName('menu')
-    if (event.target.matches('.botonMennu img')) {
-        for (let i = 0; i < dropdowns.length; i++) {
-            const openDropdown = dropdowns[i]
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show')
-            }
-        }
-    } else {
-        for (let i = 0; i < dropdowns.length; i++) {
-            const openDropdown = dropdowns[i]
-            if (!openDropdown.classList.contains('show')) {
-                openDropdown.classList.add('show')
-            }
-        }
+boton.addEventListener("click",function(uno){
+    if(contador==0){
+        enlaces.className=("menu dos")
+        contador=1;
+        boton.style.display=('none');
+        botonCerrar.style.display=('block');
+    }else{
+        enlaces.classList.remove("dos")
+        enlaces.className=("menu uno")
+        contador=0;
+        boton.style.display=('block');
+        botonCerrar.style.display=('none');
     }
-}
+})
+
+botonCerrar.addEventListener("click",function(uno){
+    if(contador==0){
+        enlaces.className=("menu dos")
+        contador=1;
+        boton.style.display=('none');
+        botonCerrar.style.display=('block');
+    }else{
+        enlaces.classList.remove("dos")
+        enlaces.className=("menu uno")
+        contador=0;
+        boton.style.display=('block');
+        botonCerrar.style.display=('none');
+    }
+})
