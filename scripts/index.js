cd4713a69de9f5a1908472759a092bc1451aa556
Bry2n <bryanmoyalima@gmail.com>
2021-01-24 12:19:57 -0800

    Version 0.2(corregido bug del boton-menu)

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,17 @@
-let boton=document.getElementById("icono");
+let boton=document.getElementById("btn-icono");
 let botonCerrar=document.getElementById("boton-cerrar");
 let enlaces=document.getElementById("menu");
+let btnIcono=document.getElementById("btn-icono");
 let contador=0;
 
 boton.addEventListener("click",function(uno){
     if(contador==0){
         enlaces.className=("menu dos")
         contador=1;
-        boton.style.display=('none');
-        botonCerrar.style.display=('block');
     }else{
         enlaces.classList.remove("dos")
         enlaces.className=("menu uno")
         contador=0;
-        boton.style.display=('block');
-        botonCerrar.style.display=('none');
     }
 })
 
-botonCerrar.addEventListener("click",function(uno){
-    if(contador==0){
-        enlaces.className=("menu dos")
-        contador=1;
-        boton.style.display=('none');
-        botonCerrar.style.display=('block');
-    }else{
-        enlaces.classList.remove("dos")
-        enlaces.className=("menu uno")
-        contador=0;
-        boton.style.display=('block');
-        botonCerrar.style.display=('none');
-    }
-})
