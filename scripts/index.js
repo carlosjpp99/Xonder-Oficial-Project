f4cc1a292a255b8fa0097d61df13a2a0607dcc86
Bry2n <bryanmoyalima@gmail.com>
2021-02-04 12:49:24 -0800

    animacion de los textos al hacer scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// Animacion para el boton de menu
+
 let boton=document.getElementById("btn-icono");
 let botonCerrar=document.getElementById("boton-cerrar");
 let enlaces=document.getElementById("menu");
@@ -13,5 +15,24 @@ boton.addEventListener("click",function(uno){
         enlaces.className=("menu uno")
         contador=0;
     }
-})
+}) 
 
+//FIN
+
+//Animacion de scroll para los textos
+
+let animacionScroll=document.querySelectorAll(".animacion-scroll-arriba");
+
+function mostrarScroll(){
+    let scrollTop1=document.documentElement.scrollTop;
+    for(let i=0;i < animacionScroll.length ;i++){
+        let alturaAnimnado=animacionScroll[i].offsetTop;
+        if(alturaAnimnado+250<scrollTop1){
+            animacionScroll[i].style.opacity=1;
+            animacionScroll[i].classList.add("mostrarAarriba");
+        }
+    }
+}
+
+window.addEventListener('scroll',mostrarScroll);
+//FIN
